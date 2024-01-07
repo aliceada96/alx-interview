ada4d8b6b3cb4dd8037bfa59916658672b21d489
aliceada96 <aliceada96@gmail.com>
2024-01-07 20:44:26 +0300

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,19 @@
+#!/usr/bin/python3
+"""Pascals  Triangle"""
+
+
 def pascal_triangle(n):
     """Returns the n-th row of Pascal's triangle as a list."""
     if n <= 0:
         return []
 
     triangle = [[1]]
-    
+
     for _ in range(1, n):
         row = [1]  # First element of each row is always 1
         for j in range(1, len(triangle[-1])):
             row.append(triangle[-1][j - 1] + triangle[-1][j])
         row.append(1)  # Last element of each row is always 1
         triangle.append(row)
-    
+
     return triangle
