3f1833cc6a832a4162e15786330e636930e86d07
aliceada96 <aliceada96@gmail.com>
2024-02-19 10:27:13 +0300

    test t

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,24 @@
 #!/usr/bin/node
+/* prints all characters of star wars movie passed as commandline argument */
 
 const request = require('request');
-// Parse the ID from the firstitional argument
+
 const movieId = process.argv[2];
-// Construct the URL the movie endpoint
 
-const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-// Make a GET request to the movie endpoint
-request(movieUrl, async (err, res, body) => {
-  if (err) {
-    console.log(err);
-  } else if (res.statusCode !== 200) {
-    console.log('Unexpected status code: ', res.statusCode);
-  } else {
-    const characterUrlsArray = JSON.parse(body).characters;
+request(url, async (err, res, body) => {
+  err && console.log(err);
 
-    // Define an async function to fetch character names
-    const fetchCharacterNames = async () => {
-      const characterPromises = characterUrlsArray.map(url => {
-        return new Promise((resolve) => {
-          request(url, (err, res, body) => {
-            if (err) {
-              console.error(err);
-            } else if (res.statusCode !== 200) {
-              console.log('Unexpected status code: ', res.statusCode);
-            } else {
-              const data = JSON.parse(body).name;
-              // Print the character name
-              console.log(data);
-              resolve(data);
-            }
-          });
-        });
+  const charactersArray = (JSON.parse(res.body).characters);
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        err && console.log(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
       });
-
-      // Wait for all character promises to resolve
-      await Promise.all(characterPromises);
-    };
-
-    // Call the async function to fetch character names
-    fetchCharacterNames();
+    });
   }
 });
