f9deba7d42b274d1a9cfe10571ac6352b01ec647
aliceada96 <aliceada96@gmail.com>
2024-02-28 14:48:38 +0300

    change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,14 +7,25 @@ def makeChange(coins, total):
     """Returns the minimum number of coins"""
     if total <= 0:
         return 0
+    
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
+    #dp = [float('inf')] * (total + 1)
+    #dp[0] = 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    ## Iterate through each coin and update the dp array
+    #for coin in coins:
+    #    for amount in range(coin, total + 1):
+    #        dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    # Iterate through each coin and update the dp array
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    # Check if it's possible to make change for the given total
-    return dp[total] if dp[total] != float('inf') else -1
+    ## Check if it's possible to make change for the given total
+    #return dp[total] if dp[total] != float('inf') else -1
